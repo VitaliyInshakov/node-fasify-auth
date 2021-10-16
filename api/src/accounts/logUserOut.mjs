b6ba5e2bc13d95a9ccd5884026696cde529d8071
Vitaliy Inshakov <michael8913@gmail.com>
2021-10-16 23:41:15 +0300

    logout fix

--- api/src/accounts/logUserOut.mjs
+++ api/src/accounts/logUserOut.mjs
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const JWTSignature = process.env.JWT_SIGNATURE;
+const { JWTSignature, ROOT_DOMAIN } = process.env;
 
 export async function logUserOut(request, reply) {
     try {
@@ -12,7 +12,17 @@ export async function logUserOut(request, reply) {
             await session.deleteOne({ sessionToken });
         }
 
-        reply.clearCookie("refreshToken").clearCookie("accessToken");
+        reply.clearCookie("refreshToken", {
+            path: "/",
+            domain: ROOT_DOMAIN,
+            httpOnly: true,
+            secure: true,
+        }).clearCookie("accessToken", {
+            path: "/",
+            domain: ROOT_DOMAIN,
+            httpOnly: true,
+            secure: true,
+        });
     } catch (e) {
         console.error(e);
     }
